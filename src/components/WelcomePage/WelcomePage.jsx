7b466f25c490407bf39c526aff8cad427a274edb
Connel Nwaodor <connel_n@hotmail.com>
2022-10-18 23:21:18 +0100

    Styled landing page

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,17 @@
 import "./WelcomePage.scss";
+import { Link } from "react-router-dom";
+import background from "../../assets/images/title-banner.png"
 
 
 const WelcomePage = () => {
   return (
-    <div>WelcomePage</div>
+    <div className="welcome">
+        <img className="welcome__image" src={background} alt="" /> <br />
+        <h3 className="welcome__message">"Don't you dare go hollow!"</h3> <br />
+        <Link style={{ textDecoration: "none" }} to="/characters">
+        <button className="welcome__button">ENTER</button>
+        </Link>
+    </div>
   )
 }
 
